554a6fd1a854e682935750d0407a81cf06dd6da6
Jasper Alakoskela <jasper.alakosekla@gmail.com>
2020-09-29 12:53:45 +0300

    upgrade tehty

--- programming.js
+++ programming.js
@@ -1,6 +1,8 @@
 
 let clickCount = 0;
 let autoClick = 0;
+let shelter = 0;
+let multiplier = 1;
 
 //Upgrades
 
@@ -12,19 +14,34 @@ function buyAutoClick() {
     }
 }
 
+function buyShelter() {
+    if (clickCount >= ((shelter + 1) * 15)) {
+        clickCount = clickCount - ((shelter + 1) * 15);
+        shelter = shelter + 1;
+        update()
+    }
+}
+
 function update() {
     document.getElementById("score").value = clickCount;
     document.title = clickCount + " Pistettä";
+
     document.getElementById("ammountAutoClick").innerHTML = "Sinulla on " + autoClick + " Auto näpäytintä";
     document.getElementById("costAutoClick").innerHTML = ((autoClick + 1) * 12) + " Pistettä";
+
+    document.getElementById("ammountShelter").innerHTML = "Sinulla on " + shelter + " Eläinhoitolaa";
+    document.getElementById("costShelter").innerHTML = ((shelter + 1) * 15) + " Pistettä";
+
+    document.getElementById("clickspersecond").innerHTML = "Saat " + (((autoClick) + (shelter * 2)) * multiplier) + " Pistettä per/s";
 }
 
 function timer() {
     clickCount = clickCount + autoClick;
+    clickCount = clickCount + shelter * 2;
     update()
 }
 
-setInterval(timer, 3000)
+setInterval(timer, 1000)
 
 
 //kuvat
@@ -55,11 +72,15 @@ function add() {
 function save() {
     localStorage.setItem("clickCount", clickCount);
     localStorage.setItem("autoClick", autoClick);
+    localStorage.setItem("shelter", shelter);
+
 }
 function load() {
     clickCount = localStorage.getItem("clickCount");
     clickCount = parseInt(clickCount);
     autoClick = localStorage.getItem("autoClick");
     autoClick = parseInt(autoClick);
+    shelter = localStorage.getItem("shelter");
+    shelter = parseInt(shelter);
     update()
 }
